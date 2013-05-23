ea09665032e9afe592e64c245ca557b39f17a3e4
Felix Rodriguez <felix.a.rod@gmail.com>
2013-05-23 11:03:41 -0700

    update config

--- config.js
+++ config.js
@@ -5,3 +5,5 @@ config.github_pass = process.env.GITHUB_PASS || 'password';
 
 config.lighthouse_username = process.env.LIGHTHOUSE_USER || 'username';
 config.lighthouse_pass = process.env.LIGHTHOUSE_PASS || 'password';
+
+module.exports = config;
